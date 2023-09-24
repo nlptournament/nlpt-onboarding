672014765758de2378e82ecb43ecb038325ab120
Nils Ost <102733481+nils-ost@users.noreply.github.com>
2023-09-24 10:27:44 +0200

    explicit conversation of selectedSeat to int, before sending it to backend

--- src/app/components/onboarding/onboarding.component.ts
+++ src/app/components/onboarding/onboarding.component.ts
@@ -46,6 +46,7 @@ export class OnboardingComponent implements OnInit {
   sendSeatParameters() {
     if (this.selectedTable && this.selectedSeat && this.selectedPw) {
       this.errorMsg = [];
+      this.selectedSeat = parseInt(this.selectedSeat);
       this.onboardingService
         .startOnboarding(this.selectedTable, this.selectedSeat, this.selectedPw)
         .subscribe({
